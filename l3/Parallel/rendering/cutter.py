8b87bc5bb18de9fbee0db19ed960a2deec10142d
Arthur Asylgareev <son.of.gallifrei@gmail.com>
2013-12-05 20:30:40 +0400

    Cutter is better now

--- l3/Parallel/rendering/cutter.py
+++ l3/Parallel/rendering/cutter.py
@@ -15,7 +15,9 @@ def chunks(lst, count):
 def cutter(filename, numtimes):
     times = []
     ff = fileinput.FileInput(filename)
-    linenumbers = 1;
+    lasttime = None
+    linenumbers = 1
+    tempfile = None
     print("Looking through %s: matching times" % filename)
     for line in ff:
         buf = line.split(";", 1)
@@ -23,22 +25,31 @@ def cutter(filename, numtimes):
             if int(buf[0]) not in times:
                 times.append(int(buf[0]))
         linenumbers += 1
-
+    print("Finished with matching!\n Preparing to print!")
     #times = chunks(times, numtimes)
     files = ["output%d.csv" % i for i in range(len(times))]
-    pfiles = [open(files[i], "wb") for i in range(len(times))]
+    #pfiles = [open(files[i], "wb") for i in range(len(times))]
 
     ff = fileinput.FileInput(filename)
     print("Starting printing information to file. \n It'll take a while, go and drink some tea")
     for line in ff:
         if ff.filelineno() == 1:
-            for outfile in pfiles:
-                outfile.write(bytes(line, "utf-8"))
+            for tempfilename in files:
+                tempfile=open(tempfilename, "wb")
+                tempfile.write(bytes(line, "utf-8"))
+                tempfile.close()
         else:
              buf = line.split(";", 1)
-             pfiles[get_index(times, int(buf[0]))].write(bytes(line, "utf-8"))
+             bufT = int(buf[0])
+             if lasttime != bufT:
+                tempfile.close()
+                tempfile=open(files[get_index(times, bufT)], "wb")
+                tempfile.write(bytes(line, "utf-8"))
+                lasttime = bufT
+             else:
+                 tempfile.write(bytes(line, "utf-8"))
+    tempfile.close()
 
-    for outfile in pfiles:
-        outfile.close()
+
     print("Ended with printing!")
     return files
